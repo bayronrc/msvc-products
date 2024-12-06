43bab894a072de659834960965a948187debbdd2
bayronrc <ryshen98@outlook.com>
2024-12-06 17:18:38 -0500

    first commit

---
+++ src/main/java/com/bayron/springcloud/msvc/products/entities/Product.java
@@ -0,0 +1,34 @@
+package com.bayron.springcloud.msvc.products.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String name;
+
+    @Column(length = 100)
+    private Double price;
+    
+    @Lob
+    private String description;
+
+}
