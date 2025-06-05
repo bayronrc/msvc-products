cfba5be7451f4d2d7d733d5e50ddbc18edf749c4
bayronrc <ryshen98@outlook.com>
2025-06-05 18:04:21 -0500

    Modify: ProductController route

--- src/main/java/com/bayron/springcloud/msvc/products/controllers/ProductController.java
+++ src/main/java/com/bayron/springcloud/msvc/products/controllers/ProductController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping(path = "/products")
 public class ProductController {
 
     final ProductService service;
